0c01c5c81f4fc797c67a5139a214f79e19f7571e
salihbezai <salihbezai98@gmail.com>
2024-11-11 00:11:40 +0100

    setup upload pdf file

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,17 +1,49 @@
+import ChatWrapper from '@/components/ChatWrapper';
+import PdfRenderer from '@/components/PdfRenderer';
+import { db } from '@/db';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { notFound, redirect } from 'next/navigation';
 import React from 'react'
 
 interface PageProps {
     params: {
-        fileid: string
+      fileid: string
     }
 }
 
-const Page = ({ params }:PageProps) => {
+const Page = async ({ params }:PageProps) => {
     // retreive the file id 
     const { fileid } = params;
-    // make database call
-  return (
-    <div>{fileid}</div>
+
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+ 
+    if(!user || !user.id) redirect(`/auth-calback?origin=dashboard/${fileid}`)
+
+      // make database call
+    const file = await db.file.findFirst({
+      where:{
+        id: fileid,
+        userId: user.id
+      }
+    })
+
+    if(!file) notFound()
+      return (
+    <div className='flex-1 justify-between flex-col h-[calc(100vh-3.5rem)]:'>
+      <div className='mx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
+        {/*left side*/}
+        <div className='flex-1 xl:flex'>
+          <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
+            <PdfRenderer />
+          </div>
+        </div>
+        <div className='shrink-0 flex-[0.75] 
+        border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
+          <ChatWrapper />
+        </div>
+      </div>
+    </div>
   )
 }
 
